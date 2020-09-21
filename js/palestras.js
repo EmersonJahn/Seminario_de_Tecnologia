e8d9779c877dc9a529d5d4656a72945e9adac180
donelli <49295044+donelli@users.noreply.github.com>
2020-09-21 12:29:59 -0300

    telas de palestras dos dias

--- js/palestras.js
+++ js/palestras.js
@@ -3,16 +3,18 @@ $(() => {
   $("#cursoSelect").change((event) => {
       const valFiltro = $("#cursoSelect").val();
       
-      $("#container-dados .row.linha-principal").filter(() => {
-          
-          const curso = $(this)[0].children[3].children[1].innerHTML;
-          
-          if (valFiltro == 'Todos' || valFiltro === curso) {
-              $(this).toggle(true);
-          } else {
-              $(this).toggle(false);
-          }
-          
+      $("#container-dados .row.linha-principal").each((index, element) => {
+        
+        const curso = $(element)[0].children[3].children[1].innerHTML;
+        
+        $(element).collapse({ toggle: false });
+        
+        if (valFiltro == 'Todos' || valFiltro === curso) {
+            $(element).toggle(true);
+        } else {
+            $(element).toggle(false);
+        }
+        
       });
       
   });
